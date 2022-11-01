0a65161430896ca85f33128c0978b2d544e94690
chantell <chantellgebauer@gmail.com>
2022-11-01 17:00:56 +0100

    backConCrudYApi

---
+++ src/Entity/Proyectos.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProyectosRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
+
+
+#[ORM\Entity(repositoryClass: ProyectosRepository::class)]
+#[ApiResource]
+class Proyectos
+{
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $fecha = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lenguajes = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $descripcion = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getFecha(): ?\DateTimeInterface
+    {
+        return $this->fecha;
+    }
+
+    public function setFecha(\DateTimeInterface $fecha): self
+    {
+        $this->fecha = $fecha;
+
+        return $this;
+    }
+
+    public function getLenguajes(): ?string
+    {
+        return $this->lenguajes;
+    }
+
+    public function setLenguajes(string $lenguajes): self
+    {
+        $this->lenguajes = $lenguajes;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+}
